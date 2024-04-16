b1e8345f9d369a9fc3f4c1d40b1f77ca6170af51
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-04-16 11:13:01 +0530

    Feat: Add reducer setup

--- src/Store/Reducers.jsx
+++ src/Store/Reducers.jsx
@@ -4,4 +4,29 @@ const initialState = {
     playerData: ''
 }
 
-export const reducer = test
+export const reducer = (state = initialState, action) => {
+    if (action.type.includes("pending")) {
+
+        return state;
+    }
+
+    if (action.type.includes("rejected")) {
+
+        return state;
+    }
+
+    switch (action.type) {
+        case `${types.GET_PLAYER_INFO}/fulfilled`:
+            return {
+                ...state,
+                playerData: action.payload
+            };
+        case `${types.GET_PLAYER_INFO}/rejected`:
+            return {
+                ...state,
+                playerData: action.payload
+            };
+        default:
+            return state;
+    }
+}
