94be83e978644c95971082cc535e430257415680
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2024-04-16 16:57:41 +0530

    Feat: Add buttons in Navbar

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,14 +1,39 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { IconButton, Button, Box, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { IconButton, Text, Button, Box, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 const Navbar = (props) => {
     const { toggleColorMode } = useColorMode();
     const iconColor = useColorModeValue('gray.600', 'gray.300');
 
+    const handleHome = () => {
+        console.log("test")
+    }
+
+    const handleLowBatteryTeam = () => {
+        console.log("test")
+    }
+
+    const handleSearch = () => {
+        console.log("test")
+    }
+
+    const handleProfile = () => {
+        console.log("test")
+    }
+
+    const handleSignIn = () => {
+        console.log("test")
+    }
+
     return (
-        <Box padding='1rem' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box style={{ padding: '1rem', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+            <Button variant='ghost' mr='0.5rem' onClick={handleHome}>Home</Button>
+            <Button variant='ghost' mr='0.5rem' onClick={handleLowBatteryTeam}>Low Battery Team</Button>
+            <Button variant='ghost' mr='0.5rem' onClick={handleSearch}>Search</Button>
+            <Button variant='ghost' mr='0.5rem' onClick={handleProfile}>Profile</Button>
+            <Button variant='ghost' mr='0.5rem' onClick={handleSignIn}>Sign In</Button>
             <IconButton
                 aria-label="Toggle dark mode"
                 icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
